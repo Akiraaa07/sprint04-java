ce938e5cfc8e0fe142f967c9d9f2b2bb8f8ad0cf
Akiraaa07 <igor.tateishi@gmail.com>
2025-05-12 02:29:59 -0300

    Renomeia classe principal para Sprint04Application

--- src/main/java/br/com/fiap/sprint03/Sprint04Application.java
+++ src/main/java/br/com/fiap/sprint03/Sprint04Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sprint03Application {
+public class Sprint04Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sprint03Application.class, args);
+		SpringApplication.run(Sprint04Application.class, args);
 	}
 
 }
